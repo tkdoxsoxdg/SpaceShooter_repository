f2e337119ee27d294f2fa5f6a5b39193a947edc6
tkd\YukiTakada <y.tkdoxsoxdg@gmail.com>
2017-07-08 17:11:09 +0900

    17:10 commit takada yuki

--- Space Shooter/Assets/Scripts/PlyerController.cs
+++ Space Shooter/Assets/Scripts/PlyerController.cs
@@ -12,6 +12,8 @@ public class PlyerController : MonoBehaviour {
 
     private Rigidbody rb;
     public float speed;
+    public Boundary boundary;
+   
 
     void Start()
     {
@@ -25,14 +27,15 @@ public class PlyerController : MonoBehaviour {
         float moveVertical = Input.GetAxis("Vertical");
 
         rb.position = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        rb.velocity = new Vector3(moveHorizontal, 0.0f, moveVertical) * speed;
+        rb.velocity = rb.position * speed;
 
-        rb.rotation = Quaternion.Euler(new Vector3(moveHorizontal, 0.0f, moveVertical));
-  
-        }
+        rb.position = new Vector3
+            (
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+            );
 
-
-
-
+    }
     }
 
