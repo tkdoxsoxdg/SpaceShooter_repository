2c472af63b2b37db810147f2a63b927aec4337b1
MYCOMPUTER\IFK_YukiTakada <y.tkdoxsoxdg@gmail.com>
2017-08-06 12:15:06 +0900

    commit 2017/07/30

--- Space Shooter/Assets/Scripts/DestroyByContact.cs
+++ Space Shooter/Assets/Scripts/DestroyByContact.cs
@@ -6,6 +6,8 @@ public class DestroyByContact : MonoBehaviour
 {
     public GameObject explosion;
     public GameObject playerExplosion;
+	private GameController gameController;
+	public int score;
     //OnTrigger→今回はBoundaryのコライダーを使う→タグ付け判定→対象のタグ付けしたオブジェクトを返す。
     void OnTriggerEnter(Collider other)
     {
@@ -17,8 +19,11 @@ public class DestroyByContact : MonoBehaviour
         if (other.tag == "Player")
         {
             Instantiate(playerExplosion, other.transform.position, other.transform.rotation);// as GameObject;
+			//gameController.GameOver();
+	
         }
         //other.gameObject : Anteroid   gameobject : Fire1
+		//gameController.AddScore(score);
         Destroy(other.gameObject);
         Destroy(gameObject);
 
--- Space Shooter/Assets/Scripts/GameController.cs
+++ Space Shooter/Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
     public GameObject hazard;
@@ -10,10 +11,9 @@ public class GameController : MonoBehaviour {
     public float startWait;
     public float waveWait;
 
-	public Text	ScoreText;
-	public Text RestartText;
-	piblic Text GameOverText;
-
+	public GUIText gameOverText;
+	public GUIText restartText;
+	public GUIText scoreText;
 
 	private bool gameOver;
 	private bool restart;
@@ -23,27 +23,58 @@ public class GameController : MonoBehaviour {
     void Start()
     {
         StartCoroutine(SpawnWaves());
-		restart = false;
 		gameOver = false;
-		RestartText.text = "";
-		GameOverText = "";
+		restart = false;
+		gameOverText.text = "";
+		restartText.text = "";
 		score = 0;
+		UpdateScore ();
+
+
     }
+	void Update(){
+		if (restart) {
+			if (Input.GetKeyDown (KeyCode.R)) {
+				SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);
+			}
+		}
+	}
 
     IEnumerator SpawnWaves()
-    {
-        yield return new WaitForSeconds(startWait);
-        while (true)
-        {
-            for (int i = 0; i < hazardcount; i++)
-            {
-                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
-                Quaternion spawnRotation = Quaternion.identity;
-                Instantiate(hazard, spawnPosition, spawnRotation);
-                yield return new WaitForSeconds(spawnWait);
-            }
-            yield return new WaitForSeconds(waveWait);
-        }
+	{
+		yield return new WaitForSeconds (startWait);
+		while (true) {
+			for (int i = 0; i < hazardcount; i++) {
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+        
+			if (gameOver) {
+				restartText.text = "Press 'R' for Restart";
+				restart = true;
+				break;
+			}
 
-    }
+		}
+	}
+
+	public void AddScore (int addScoreValue)
+	{
+		score += addScoreValue;
+		//Debug.Log(score);
+		UpdateScore ();
+	}
+	void UpdateScore()
+	{
+		scoreText.text = "Score:" + score;
+	}
+	public void GameOver(){
+		gameOverText.text = "Game Over!!!";
+		gameOver = true;
+	
+	}
+		
 }
