35d8205db362d15611a515ad5dc8484a626251d6
MYCOMPUTER\IFK_YukiTakada <y.tkdoxsoxdg@gmail.com>
2017-08-27 18:27:23 +0900

    2017/08/26 Extending Space Shooter: Enemies, More Hazards, Scrolling BG...の章に突入。
    作成したゲームオブジェクトがプレイモードにて、画面から消える現象が発生（Unity 2017のUI仕様かも）
    スクリプト上では、問題ないかと思わる。

---
+++ Space Shooter/Assets/Scripts/BGScroller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroller : MonoBehaviour {
+    public float scrollspeed;
+    public float tileSizeZ;
+
+    public Vector3 startPosition;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float newPosition = Mathf.Repeat(Time.time * scrollspeed, tileSizeZ);
+        transform.position = startPosition + Vector3.forward * newPosition;
+	}
+}
--- Space Shooter/Assets/Scripts/GameController.cs
+++ Space Shooter/Assets/Scripts/GameController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
-    public GameObject hazard;
+    public GameObject[] hazards;
     public Vector3 spawnValues;
     public int hazardcount;
     public float spawnWait;
@@ -48,6 +48,7 @@ public class GameController : MonoBehaviour {
 		yield return new WaitForSeconds (startWait);
 		while (true) {
 			for (int i = 0; i < hazardcount; i++) {
+                GameObject hazard = hazards[Random.Range(0, hazards.Length)];
 				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
 				Quaternion spawnRotation = Quaternion.identity;
 				Instantiate (hazard, spawnPosition, spawnRotation);
