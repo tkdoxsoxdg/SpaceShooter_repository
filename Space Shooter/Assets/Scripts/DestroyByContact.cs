0ca4f86aa77a9b08944094e4f343e7f9f8a74e68
tkd\YukiTakada <y.tkdoxsoxdg@gmail.com>
2017-10-08 17:00:14 +0900

    Revert "山崎さんへ"
    This reverts commit 3783ab308530d48e87d6e43eb247dd2709bec0e3.

--- Space Shooter/Assets/Scripts/DestroyByContact.cs
+++ Space Shooter/Assets/Scripts/DestroyByContact.cs
@@ -6,41 +6,19 @@ public class DestroyByContact : MonoBehaviour
 {
     public GameObject explosion;
     public GameObject playerExplosion;
-	public int scoreValue;
-    private GameController gameController;
     //OnTrigger→今回はBoundaryのコライダーを使う→タグ付け判定→対象のタグ付けしたオブジェクトを返す。
-    void Start()
-    {
-        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
-        if(gameControllerObject != null)
-        {
-            gameController = gameControllerObject.GetComponent<GameController>();
-        }
-        if(gameControllerObject == null)
-        {
-            Debug.Log("Can't find 'GameController' script");
-        }
-    }
-
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Boundary") || other.CompareTag("Enemy"))
+        if (other.tag == "Boundary")
         {
             return;
         }
-
-        if (explosion != null)
-        {
-            Instantiate(explosion, transform.position, transform.rotation);
-        }
+        Instantiate(explosion, transform.position, transform.rotation);
         if (other.tag == "Player")
         {
             Instantiate(playerExplosion, other.transform.position, other.transform.rotation);// as GameObject;
-			gameController.GameOver();
-
         }
         //other.gameObject : Anteroid   gameobject : Fire1
-        gameController.AddScore(scoreValue);
         Destroy(other.gameObject);
         Destroy(gameObject);
 
